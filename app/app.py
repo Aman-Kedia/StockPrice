80f11cec0a2aad425c48c2d5fe6ef356d7d9423e
Aman Kedia <amankedia1402@gmail.com>
2021-08-31 19:05:56 +0530

    Updated

--- app/app.py
+++ app/app.py
@@ -11,11 +11,15 @@ def handler(event, context):
     password = db_password
     database_name = db_name
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
-    url = ['https://in.finance.yahoo.com/quote/RELIANCE.NS/', 'https://in.finance.yahoo.com/quote/TCS.NS/', 
-    'https://in.finance.yahoo.com/quote/INFY.NS/', 'https://in.finance.yahoo.com/quote/HDFC.NS/', 'https://finance.yahoo.com/quote/ADANIENT.NS/']
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
+    
+    url = ['https://www.google.com/finance/quote/RELIANCE:NSE?sa=X&ved=2ahUKEwin6NnOztryAhWNc30KHVImBA4Q_AUoAXoECAEQAw',
+           'https://www.google.com/finance/quote/ADANIENT:NSE?sa=X&ved=2ahUKEwjHuqavztryAhULfisKHXY_CoQQ_AUoAXoECAEQAw',
+           'https://www.google.com/finance/quote/TCS:NSE?sa=X&ved=2ahUKEwjAoNa0ztryAhXOV30KHY2ACc0Q_AUoAXoECAEQAw', 
+           'https://www.google.com/finance/quote/INFY:NSE?sa=X&ved=2ahUKEwi4yoS4ztryAhXLXisKHchYA3gQ_AUoAXoECAEQAw',
+           'https://www.google.com/finance/quote/HDFC:NSE?sa=X&ved=2ahUKEwiWg4i7ztryAhXSdn0KHRrFDPwQ_AUoAXoECAEQAw']
 
-    stocks = ['Reliance', 'TCS', 'Infosys', 'HDFC', 'AdaniEnt']
+    stocks = ['Reliance', 'AdaniEnt', 'TCS', 'Infosys', 'HDFC']
     
     connection = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name, port=3306)
     cursor = connection.cursor()
@@ -23,24 +27,26 @@ def handler(event, context):
     for i in range(5):
         r = requests.get(url[i], headers=headers)
         soup = BeautifulSoup(r.text, 'html.parser')
+        
+        price = soup.find('div', {'class': 'YMlKec fxKbKc'}).text
 
-        price = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')[0].text
-        curtime = soup.find('div', {'class': 'C($tertiaryColor) D(b) Fz(12px) Fw(n) Mstart(0)--mobpsm Mt(6px)--mobpsm'}).find_all('span')[0].text
+        curtime = soup.find('div', {'class': 'ygUjEc'}).text
         curtime = list(curtime.split())
-        if curtime[0] == "As":
-            curtime = curtime[2][:-2]
+        
+        if len(curtime[2])==7:
+            curtime = curtime[2][:5] + '00'
         else:
-            curtime = curtime[-2][:-2]
+            curtime = curtime[2][:6] + '00'
+    
         x = curtime.find(':')
         if curtime[:x] in ['1','2','3']:
-            curtime = str(int(curtime[0])+12) + curtime[1:]
-        else:
+            curtime = str(int(curtime[:x])+12) + curtime[x:]
+        elif curtime[:x] == '9':
             curtime = '0' + curtime
-        curtime += ':00'
         price = str(price)
-        price = price[0] + price[2:]
+        price = price[1] + price[3:]
         price = float(price)
-        
+
         cursor.execute(f'SELECT Price, Ctime from {stocks[i]} where id = (SELECT max(id) from {stocks[i]})')
         rows = cursor.fetchall()
 
